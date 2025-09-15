6ca71db2525e3e3a45b75ab5c0b1572e85eb2547
Sergio AHOUANGONOU <sergio.ahouangonou@chem.loc>
2025-09-15 19:07:41 +0100

    Déclaration des variables couleurs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/NavbarComponent.vue'
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
-
+  <div class="p-4">
+    <h1 class="bg-blueColor">Salut</h1>
+    <div class="bg-bgColor text-white p-4">Couleur brand</div>
+    <div class="bg-redColor text-black p-4">Couleur brand light</div>
+    <div class="bg-[#FAFAFBFF]">Couleur brand dark</div>
+  </div>
 </template>
+
+
 
 <style scoped>
 
