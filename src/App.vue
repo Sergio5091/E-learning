3ff7184b9af7a10d02e4d850ab7482e2789e52d0
Wariss Achamou Adeniyi BADA <wariss.bada@chem.loc>
2025-09-16 17:37:50 +0100

    Warissbada

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
+import DashboardComponent from './components/DashboardComponent.vue';
+
 </script>
 
 <template>
-  <div class="p-4">
+  <!-- <div class="p-4">
     <h1 class="bg-blueColor">Salut</h1>
     <div class="bg-bgColor text-white p-4">Couleur brand</div>
     <div class="bg-redColor text-black p-4">Couleur brand light</div>
     <div class="bg-[#FAFAFBFF]">Couleur brand dark</div>
-  </div>
+  </div> -->
+  <DashboardComponent/>
 </template>
-<div>
-  azerty
-</div>
-
-
 
 
 <style scoped>
