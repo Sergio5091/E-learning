171b760aec529f7a58768549f958d3b2df14e5a8
Alexio KOGBETO <alexio.kogbeto@chem.loc>
2025-09-18 09:47:45 +0100

    shit

--- src/components/CoursesComponents/CoursesComponent.vue
+++ src/components/CoursesComponents/CoursesComponent.vue
@@ -5,11 +5,16 @@ import CoursesListComponent from './CoursesListComponent.vue';
 import SideBarFilterComponent from './SideBarFilterComponent.vue';
 import { computed } from 'vue';
 const MainCourses = ref(data.courses)
+console.log(MainCourses.value);
+
+const CoursesTab = ref([...MainCourses.value])
+console.log(CoursesTab.value);
+
 const selectedCategory = ref('')
 
 const filteredCourses = computed(() => {
     if(!selectedCategory.value) return MainCourses.value
-    return MainCourses.value.filter((el) => el.category === selectedCategory.value )
+    return CoursesTab.value.filter((el) => el.category === selectedCategory.value )
 })
 
 
@@ -23,7 +28,7 @@ const filteredCourses = computed(() => {
 
 <template>
 <div class="flex">
-    <SideBarFilterComponent :tabCourses="MainCourses" @update:category="selectedCategory =$event"  />
+    <SideBarFilterComponent :tabCourses="CoursesTab" @update:category="selectedCategory =$event"  />
     <CoursesListComponent :tabCourses="filteredCourses" />
 </div>
 
